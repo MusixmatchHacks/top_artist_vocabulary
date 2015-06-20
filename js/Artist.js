b4d72185c5fff9038f0da6662cda56908b622757
nashmaniac <nishantve1@gmail.com>
2015-06-20 18:54:32 +0530

    Pulled images from the right folder

--- js/Artist.js
+++ js/Artist.js
@@ -21,7 +21,7 @@
 		// We will have to lazy load images for better performace, but lets ignore it for now 
 		// Add artist image 
 		// this.$artist.css('background-image', 'url(img/artist_images_smaller/' + this.selector + '.jpg)');
-		this.$artist.css('background-image', 'url(img/artist_images_40px/' + this.selector + '.jpg)');
+		this.$artist.css('background-image', 'url(img/artist_images_smaller/' + this.selector + '.jpg)');
  		// Initialize events 
 		this.events.click.call(this);
 		this.events.hover.call(this);
