bce1451add1f6112ad1f89885d32dc436e76106a
nashmaniac <nishantve1@gmail.com>
2015-06-29 18:57:50 +0530

    Replaced 40px images with 80px

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (function() {
 
+    // Set up the data and the charts
+
     // Add ranks to the data, data will be already loaded in the browser rest assured
     // data is presorted by the length of vocabulary
     vocab_data.forEach(function(data, index) {
@@ -19,5 +21,13 @@
     var allArtistsChart = AllArtists.createChart({
         allChartCssId : 'allArtists'
     }, Object.keys(plot.artists));
+
+
+
+    // contract the whole list div
+    // $('#allArtists').();
+    $('.letterArtistImage').on('mouseover', function() {
+           $(this).tipsy({text : 'id'});
+    });
 
 })();
