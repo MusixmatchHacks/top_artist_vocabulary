4580f49b409c52aaf7ee259ed65daea21f2adc75
nashmaniac <nishantve1@gmail.com>
2015-06-30 22:06:27 +0530

    Started working on polyglots

---
+++ js/ExpandButton.js
@@ -0,0 +1,62 @@
+(function() {
+	
+	// Add contractability to the allArtists chart
+	var $window = $(window);
+
+	expandButton = {
+		init: function(config) {
+			this.button = config.button;
+			this.target = config.target;
+			// Variables that'll help in maintaining the positions while scrolling
+			this.initialButtonPos = this.button.offset();
+			this.scrollLimits = this.target.offset();
+			// Initialize the click listener 
+			this.button.on('click', this.expandList);
+		},
+
+		expandList: function() {
+			expandButton.target.toggleClass('expanded');
+			if (expandButton.target.hasClass('expanded')) {
+				expandButton.target.children('.overlay').remove();
+				$(this).text('SHOW LESS');
+			} else {
+				expandButton.target.prepend($('<div></div>', {
+					class: 'overlay'
+				}));
+				$(this).text('SHOW ALL');
+				// Scroll to the next article section
+				if ($window.scrollTop() > expandButton.getLowerScrollLimit())
+					$window.scrollTop(950);
+			}
+
+		},
+
+		getUpperScrollLimit: function() {
+			return this.scrollLimits.top;
+		},
+
+		getLowerScrollLimit: function() {
+			return (this.scrollLimits.top + this.target.height() - this.button.height() - 100);
+		}
+
+
+	};
+
+
+	// Set up the scroll listener on the window to maintain the position of the button
+	// Not the best solution but will work for now ...
+	// TODO : fix this 
+	$window.on('scroll', function() {
+		var scrollPos = $window.scrollTop();
+		if (scrollPos > expandButton.getUpperScrollLimit() && scrollPos < expandButton.getLowerScrollLimit()) {
+			expandButton.button.addClass('buttonFixedScroll');
+			expandButton.button.css({
+				top: (expandButton.initialButtonPos.top - 490) + 'px',
+				left: expandButton.initialButtonPos.left + 'px'
+			});
+		} else {
+			expandButton.button.removeClass('buttonFixedScroll');
+			expandButton.button.offset(expandButton.initialButtonPos);
+		}
+	});
+})();
--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 (function() {
-
-    // Set up the data and the charts
-
     // Add ranks to the data, data will be already loaded in the browser rest assured
     // data is presorted by the length of vocabulary
+    // Some day I won't have to do this, rank will already be in the data #acchedin
     vocab_data.forEach(function(data, index) {
         data.rank = (index + 1);
     });
 
     // Initialize the plot for the main visulaization
     var plot = VocabPlot.newPlot({
-        // Css selectors of the elments on the plot
+        // Css selectors for the elments on the plot
         cssId: 'vocab_plot',
         scaleCssId: 'vocab_plot_scale',
         searchFieldCssId: 'locateArtist',
@@ -19,8 +17,9 @@
     });
 
     // Plot needs to be initialized before initializing this
+    // Initializing the list that shows all the artists
     var allArtistsChart = AllArtists.createChart({
-        allChartCssId: 'allArtists'
+        allChartCssId: 'allArtistsContainer'
     }, Object.keys(plot.artists));
 
     // Add tooltips to the artists in the AllArtist chart
@@ -36,68 +35,41 @@
         });
     });
 
-    var $window = $(window);
-    // Add contractability to the all artists div
-    var expandButton = {
-        init: function(config) {
-            this.button = config.button;
-            this.target = config.target;
-            // Variables that'll help in maintaining the positions while scrolling
-            this.initialButtonPos = this.button.offset();
-            this.scrollLimits = this.target.offset();
-            // Initialize the click listener 
-            this.button.on('click', this.expandList);
-        },
-
-        expandList: function() {
-            expandButton.target.toggleClass('expanded');
-            if (expandButton.target.hasClass('expanded')) {
-                expandButton.target.children('.overlay').remove();
-                $(this).text('SHOW LESS');
-            } else {
-                expandButton.target.prepend($('<div></div>', {
-                    class: 'overlay'
-                }));
-                $(this).text('SHOW ALL');
-                // Scroll to the next article section
-                if($window.scrollTop() > expandButton.getLowerScrollLimit())
-                    $window.scrollTop(950);
-            }
-
-        },
-
-        getUpperScrollLimit: function() {
-            return this.scrollLimits.top;
-        },
-
-        getLowerScrollLimit: function() {
-            return (this.scrollLimits.top + this.target.height() - this.button.height() - 100);
-        }
-
-
-    };
-
     expandButton.init({
-       button: $('#btn_expandList'),
-       target: $('#allArtistsContainer')
+        button: $('#btn_expandList'),
+        target: $('#allArtistsContainer')
     });
 
-    // Set up the scroll listener on the window to maintain the position of the button
-    // Not the best solution but will work for now ...
-    // TODO : fix this 
-    $window.on('scroll', function() {
-        var scrollPos = $window.scrollTop();
-        if (scrollPos > expandButton.getUpperScrollLimit() && scrollPos < expandButton.getLowerScrollLimit()) {
-            expandButton.button.addClass('buttonFixedScroll');
-            expandButton.button.css({
-                top : (expandButton.initialButtonPos.top - 490) + 'px',
-                left : expandButton.initialButtonPos.left + 'px'
-            });
-        } else {
-            expandButton.button.removeClass('buttonFixedScroll');
-            expandButton.button.offset(expandButton.initialButtonPos);
+    // Data for the polyglots
+    var polyglots = [
+        {
+            name : 'Julio Iglesias',
+            languages : ['Spanish', 'German', 'English', 'French', 'Italian', 'Portuguese', 'Russian' ]
+        },
+        {
+            name : 'Andrea Bocelli',
+            languages : ['Spanish', 'English', 'French', 'Italian', 'Portuguese']
+        },
+        {
+            name : 'Bz',
+            languages : ['Japanese', 'English']
+        },
+        {
+            name : 'Gloria Estefan',
+            languages : ['English', 'Spanish', 'French']
         }
+
+    ];
+
+    // Set of all the languages, will be used for creating the buttons 
+    var polyglotLanguages = new Set();
+    polyglots.forEach(function(polyglot) {
+        polyglot.languages.forEach(function(language) {
+            polyglotLanguages.add(language);
+        });
     });
+
+    console.log(polyglotLanguages);
 
 
 })();
