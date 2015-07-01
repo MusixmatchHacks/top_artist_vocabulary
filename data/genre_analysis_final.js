93253e4c6244d2be916bdf13aa599becbe682625
neo01124 <varunjewalikar@hotmail.com>
2015-07-01 12:34:55 +0200

    genre analysis added

---
+++ data/genre_analysis_final.js
@@ -0,0 +1,127 @@
+var genre_analysis = {
+    "Alternative-Rock": {
+        "vocab_per_artist": 2974.0,
+        "vocab_len": 2974,
+        "artists": ["R.E.M."]
+    },
+    "Punk-Rock": {
+        "vocab_per_artist": 2535.0,
+        "vocab_len": 2535,
+        "artists": ["Green Day"]
+    },
+    "Reggae": {
+        "vocab_per_artist": 1867.0,
+        "vocab_len": 1867,
+        "artists": ["Bob Marley"]
+    },
+    "Latin": {
+        "vocab_per_artist": 3871.0,
+        "vocab_len": 7742,
+        "artists": ["Julio Iglesias", "Gloria Estefan"]
+    },
+    "Grunge": {
+        "vocab_per_artist": 1835.0,
+        "vocab_len": 1835,
+        "artists": ["Nirvana"]
+    },
+    "Smooth-Jazz": {
+        "vocab_per_artist": 809.0,
+        "vocab_len": 809,
+        "artists": ["Kenny G"]
+    },
+    "R&B": {
+        "vocab_per_artist": 2461.5,
+        "vocab_len": 14769,
+        "artists": ["Beyonc\u00e9 Knowles", "Mariah Carey", "Janet Jackson", "Rihanna", "Whitney Houston", "Barry White"]
+    },
+    "Hard-Rock": {
+        "vocab_per_artist": 2251.875,
+        "vocab_len": 18015,
+        "artists": ["Aerosmith", "Van Halen", "Guns N' Roses", "Bon Jovi", "Led Zeppelin", "Scorpions", "Kiss", "AC/DC"]
+    },
+    "Heavy-Metal": {
+        "vocab_per_artist": 2797.0,
+        "vocab_len": 2797,
+        "artists": ["Metallica"]
+    },
+    "Funk": {
+        "vocab_per_artist": 2627.6666666666665,
+        "vocab_len": 7883,
+        "artists": ["Prince", "Earth Wind & Fire", "Stevie Wonder"]
+    },
+    "Pop-Rock": {
+        "vocab_per_artist": 2328.5,
+        "vocab_len": 4657,
+        "artists": ["Robbie Williams", "Bruno Mars"]
+    },
+    "Country": {
+        "vocab_per_artist": 2441.0,
+        "vocab_len": 14646,
+        "artists": ["Garth Brooks", "Taylor Swift", "Kenny Rogers", "Alabama", "George Strait", "Reba McEntire"]
+    },
+    "Soul": {
+        "vocab_per_artist": 2037.0,
+        "vocab_len": 2037,
+        "artists": ["Aretha Franklin"]
+    },
+    "Synth-Rock": {
+        "vocab_per_artist": 1884.0,
+        "vocab_len": 1884,
+        "artists": ["Depeche Mode"]
+    },
+    "Rock": {
+        "vocab_per_artist": 2402.7,
+        "vocab_len": 48054,
+        "artists": ["Bz", "Linda Ronstadt", "Johnny Hallyday", "Rod Stewart", "The Rolling Stones", "Santana", "Meat Loaf", "U2", "Queen", "Tina Turner", "Dire Straits", "Eagles", "Bryan Adams", "Tom Petty", "Paul McCartney", "The Beatles", "Fleetwood Mac", "Phil Collins", "The Who", "Foreigner"]
+    },
+    "Country-Pop": {
+        "vocab_per_artist": 1801.0,
+        "vocab_len": 1801,
+        "artists": ["Shania Twain"]
+    },
+    "Hip-Hop": {
+        "vocab_per_artist": 6378.8,
+        "vocab_len": 31894,
+        "artists": ["Eminem", "Jay Z", "Tupac Shakur", "Kanye West", "The Black Eyed Peas"]
+    },
+    "Funk-Rock": {
+        "vocab_per_artist": 3636.0,
+        "vocab_len": 3636,
+        "artists": ["Red Hot Chili Peppers"]
+    },
+    "Pop": {
+        "vocab_per_artist": 2464.2083333333335,
+        "vocab_len": 59141,
+        "artists": ["Andrea Bocelli", "Celine Dion", "Barbra Streisand", "Billy Joel", "Madonna", "Elton John", "ABBA", "Frank Sinatra", "Katy Perry", "Michael Jackson", "Lady Gaga", "Barry Manilow", "Neil Diamond", "Cher", "Britney Spears", "The Carpenters", "Olivia Newton-John", "Donna Summer", "George Michael", "The Police", "Bee Gees", "Backstreet Boys", "Lionel Richie", "Spice Girls"]
+    },
+    "Glam-Metal": {
+        "vocab_per_artist": 2343.0,
+        "vocab_len": 2343,
+        "artists": ["M\u00f6tley Cr\u00fce"]
+    },
+    "Rock-and-roll": {
+        "vocab_per_artist": 2531.0,
+        "vocab_len": 2531,
+        "artists": ["Elvis Presley"]
+    },
+    "Teen-Pop": {
+        "vocab_per_artist": 1802.0,
+        "vocab_len": 1802,
+        "artists": ["New Kids on the Block"]
+    },
+    "Progressive-Rock": {
+        "vocab_per_artist": 3287.0,
+        "vocab_len": 6574,
+        "artists": ["Genesis", "Pink Floyd"]
+    },
+    "Folk": {
+        "vocab_per_artist": 4883.0,
+        "vocab_len": 4883,
+        "artists": ["Bob Dylan"]
+    },
+    "New-Age": {
+        "vocab_per_artist": 1869.0,
+        "vocab_len": 1869,
+        "artists": ["Enya"]
+    }
+}
