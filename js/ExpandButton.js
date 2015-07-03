5d9dff228603e8848c118c90b53ce4266dfa0bdb
nashmaniac <nishantve1@gmail.com>
2015-07-03 20:26:42 +0530

    Moved all list note to bottom

--- js/ExpandButton.js
+++ js/ExpandButton.js
@@ -51,7 +51,7 @@
 		if (scrollPos > expandButton.getUpperScrollLimit() && scrollPos < expandButton.getLowerScrollLimit()) {
 			expandButton.button.addClass('buttonFixedScroll');
 			expandButton.button.css({
-				top: (expandButton.initialButtonPos.top - 610) + 'px',
+				top: (expandButton.initialButtonPos.top - 548) + 'px',
 				left: expandButton.initialButtonPos.left + 'px'
 			});
 		} else {
