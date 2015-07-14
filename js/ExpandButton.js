163a578a85ffa3e61891426e6fcce622a48d9fb5
nashmaniac <nishantve1@gmail.com>
2015-07-14 18:34:53 +0530

    Improved UX of show all button

--- js/ExpandButton.js
+++ js/ExpandButton.js
@@ -15,15 +15,22 @@
 		},
 
 		expandListToggle: function() {
+			var that = expandButton;
 			expandButton.target.toggleClass('expanded');
-			if (expandButton.target.hasClass('expanded')) {
+			if (expandButton.target.hasClass('expanded')) { // The list is expanded following block contracts it
 				expandButton.target.children('.overlay').remove();
-				$(this).text('Show less');
-			} else {
+				$(this).text('SHOW LESS');
+
+				// Fix the button at the bottom of the page
+				that.attachButtonToBottom($(this));
+			} else { // Following block expands the list 
 				expandButton.target.prepend($('<div></div>', {
 					class: 'overlay'
 				}));
-				$(this).text('Show all');
+				$(this).text('SHOW ALL');
+
+				that.restoreButtonPosition($(this));
+
 				// Scroll to the next article section
 				if ($window.scrollTop() > expandButton.getLowerScrollLimit())
 					$window.scrollTop(1100);
@@ -31,12 +38,29 @@
 
 		},
 
+		attachButtonToBottom: function(button) {
+			button.addClass('attachedToBottom')
+			button.css({
+				top: ($window.height() - 45) + 'px'
+			});
+		},
+
+		restoreButtonPosition: function(button) {
+			var that = this;
+			button.removeClass('attachedToBottom');
+			console.log(that.target.height(), that.target.width(), that.target.offset().top);
+			button.css({
+				position: 'absolute',
+				top: that.target.offset().top + 164 + 'px'
+			});
+		},
+
 		getUpperScrollLimit: function() {
 			return this.scrollLimits.top;
 		},
 
 		getLowerScrollLimit: function() {
-			return (this.scrollLimits.top + this.target.height() - this.button.height() - 100);
+			return (this.scrollLimits.top + this.target.height() - this.button.height());
 		}
 
 
@@ -48,15 +72,16 @@
 	// TODO : fix this
 	$window.on('scroll', function() {
 		var scrollPos = $window.scrollTop();
-		if (scrollPos > expandButton.getUpperScrollLimit() && scrollPos < expandButton.getLowerScrollLimit()) {
-			expandButton.button.addClass('buttonFixedScroll');
-			expandButton.button.css({
-				top: (expandButton.initialButtonPos.top - 532) + 'px',
-				left: expandButton.initialButtonPos.left + 'px'
-			});
-		} else {
-			expandButton.button.removeClass('buttonFixedScroll');
-			expandButton.button.offset(expandButton.initialButtonPos);
+		if (scrollPos > (expandButton.getLowerScrollLimit() - 700)) {
+			if(expandButton.button.hasClass('attachedToBottom')){
+				expandButton.button.removeClass('attachedToBottom');
+				expandButton.button.css({
+					position: 'absolute',
+					top: expandButton.getLowerScrollLimit() - 480 + 'px'
+				});
+			}
+		}else if(scrollPos < (expandButton.getLowerScrollLimit() - 700) && $('.overlay').length === 0) {
+			expandButton.attachButtonToBottom(expandButton.button);
 		}
 	});
 })();
