48872e31378123b0c5c525fc148ef454c11991e3
neo01124 <mr.unwell2006@gmail.com>
2015-07-26 10:57:04 +0200

    share urls added

---
+++ data/genre_analysis_final_share_urls.js
@@ -0,0 +1,176 @@
+var genre_data = [{
+    "genre_name": "Alternative Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Rem-5"],
+    "total_vocab_len": 2974,
+    "avg_vocab": 2974.0,
+    "artists": ["REM"],
+    "num_artists": 1
+}, {
+    "genre_name": "Punk Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Green-Day"],
+    "total_vocab_len": 2535,
+    "avg_vocab": 2535.0,
+    "artists": ["Green Day"],
+    "num_artists": 1
+}, {
+    "genre_name": "Reggae",
+    "share_urls": ["https://www.musixmatch.com/artist/Bob-Marley"],
+    "total_vocab_len": 1867,
+    "avg_vocab": 1867.0,
+    "artists": ["Bob Marley"],
+    "num_artists": 1
+}, {
+    "genre_name": "Latin",
+    "share_urls": ["https://www.musixmatch.com/artist/Julio-Iglesias", "https://www.musixmatch.com/artist/Gloria-Estefan"],
+    "total_vocab_len": 7742,
+    "avg_vocab": 3871.0,
+    "artists": ["Julio Iglesias", "Gloria Estefan"],
+    "num_artists": 2
+}, {
+    "genre_name": "Grunge",
+    "share_urls": ["https://www.musixmatch.com/artist/Nirvana"],
+    "total_vocab_len": 1835,
+    "avg_vocab": 1835.0,
+    "artists": ["Nirvana"],
+    "num_artists": 1
+}, {
+    "genre_name": "Smooth Jazz",
+    "share_urls": ["https://www.musixmatch.com/artist/Kenny-G"],
+    "total_vocab_len": 809,
+    "avg_vocab": 809.0,
+    "artists": ["Kenny G"],
+    "num_artists": 1
+}, {
+    "genre_name": "R&B",
+    "share_urls": ["https://www.musixmatch.com/artist/Beyonce-Knowles", "https://www.musixmatch.com/artist/Mariah-Carey", "https://www.musixmatch.com/artist/Janet-Jackson", "https://www.musixmatch.com/artist/Rihanna", "https://www.musixmatch.com/artist/Whitney-Houston", "https://www.musixmatch.com/artist/Barry-White"],
+    "total_vocab_len": 14769,
+    "avg_vocab": 2461.5,
+    "artists": ["Beyonc\u00e9 Knowles", "Mariah Carey", "Janet Jackson", "Rihanna", "Whitney Houston", "Barry White"],
+    "num_artists": 6
+}, {
+    "genre_name": "Hard Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Aerosmith", "https://www.musixmatch.com/artist/Van-Halen", "https://www.musixmatch.com/artist/Guns-N-Roses", "https://www.musixmatch.com/artist/Bon-Jovi", "https://www.musixmatch.com/artist/Led-Zeppelin", "https://www.musixmatch.com/artist/Scorpions", "https://www.musixmatch.com/artist/Kiss-2", "https://www.musixmatch.com/artist/AC-DC"],
+    "total_vocab_len": 18015,
+    "avg_vocab": 2251.875,
+    "artists": ["Aerosmith", "Van Halen", "Guns N Roses", "Bon Jovi", "Led Zeppelin", "Scorpions", "Kiss", "ACDC"],
+    "num_artists": 8
+}, {
+    "genre_name": "Heavy Metal",
+    "share_urls": ["https://www.musixmatch.com/artist/Metallica"],
+    "total_vocab_len": 2797,
+    "avg_vocab": 2797.0,
+    "artists": ["Metallica"],
+    "num_artists": 1
+}, {
+    "genre_name": "Funk",
+    "share_urls": ["https://www.musixmatch.com/artist/Prince", "https://www.musixmatch.com/artist/Earth-Wind-Fire", "https://www.musixmatch.com/artist/Stevie-Wonder"],
+    "total_vocab_len": 7883,
+    "avg_vocab": 2627.6666666666665,
+    "artists": ["Prince", "Earth Wind and Fire", "Stevie Wonder"],
+    "num_artists": 3
+}, {
+    "genre_name": "Pop Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Robbie-Williams", "https://www.musixmatch.com/artist/Bruno-Mars"],
+    "total_vocab_len": 4657,
+    "avg_vocab": 2328.5,
+    "artists": ["Robbie Williams", "Bruno Mars"],
+    "num_artists": 2
+}, {
+    "genre_name": "Country",
+    "share_urls": ["https://www.musixmatch.com/artist/Garth-Brooks", "https://www.musixmatch.com/artist/Taylor-Swift", "https://www.musixmatch.com/artist/Kenny-Rogers", "https://www.musixmatch.com/artist/Alabama", "https://www.musixmatch.com/artist/George-Strait", "https://www.musixmatch.com/artist/Reba-McEntire"],
+    "total_vocab_len": 14646,
+    "avg_vocab": 2441.0,
+    "artists": ["Garth Brooks", "Taylor Swift", "Kenny Rogers", "Alabama", "George Strait", "Reba McEntire"],
+    "num_artists": 6
+}, {
+    "genre_name": "Soul",
+    "share_urls": ["https://www.musixmatch.com/artist/Aretha-Franklin"],
+    "total_vocab_len": 2037,
+    "avg_vocab": 2037.0,
+    "artists": ["Aretha Franklin"],
+    "num_artists": 1
+}, {
+    "genre_name": "Synth Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Depeche-Mode"],
+    "total_vocab_len": 1884,
+    "avg_vocab": 1884.0,
+    "artists": ["Depeche Mode"],
+    "num_artists": 1
+}, {
+    "genre_name": "Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/The-Beatles", "https://www.musixmatch.com/artist/The-Rolling-Stones", "https://www.musixmatch.com/artist/U2", "https://www.musixmatch.com/artist/Queen", "https://www.musixmatch.com/artist/Eagles", "https://www.musixmatch.com/artist/The-Who", "https://www.musixmatch.com/artist/B\u2019z", "https://www.musixmatch.com/artist/Linda-Ronstadt", "https://www.musixmatch.com/artist/Johnny-Hallyday", "https://www.musixmatch.com/artist/Rod-Stewart", "https://www.musixmatch.com/artist/Santana", "https://www.musixmatch.com/artist/Meat-Loaf", "https://www.musixmatch.com/artist/Tina-Turner", "https://www.musixmatch.com/artist/Dire-Straits", "https://www.musixmatch.com/artist/Bryan-Adams", "https://www.musixmatch.com/artist/Tom-Petty", "https://www.musixmatch.com/artist/Paul-McCartney", "https://www.musixmatch.com/artist/Fleetwood-Mac", "https://www.musixmatch.com/artist/Phil-Collins", "https://www.musixmatch.com/artist/Foreigner"],
+    "total_vocab_len": 48054,
+    "avg_vocab": 2402.7,
+    "artists": ["The Beatles", "The Rolling Stones", "U2", "Queen", "Eagles", "The Who", "Bz", "Linda Ronstadt", "Johnny Hallyday", "Rod Stewart", "Santana", "Meat Loaf", "Tina Turner", "Dire Straits", "Bryan Adams", "Tom Petty", "Paul McCartney", "Fleetwood Mac", "Phil Collins", "Foreigner"],
+    "num_artists": 20
+}, {
+    "genre_name": "Country Pop",
+    "share_urls": ["https://www.musixmatch.com/artist/Shania-Twain"],
+    "total_vocab_len": 1801,
+    "avg_vocab": 1801.0,
+    "artists": ["Shania Twain"],
+    "num_artists": 1
+}, {
+    "genre_name": "Hip Hop",
+    "share_urls": ["https://www.musixmatch.com/artist/Eminem", "https://www.musixmatch.com/artist/JAY-Z", "https://www.musixmatch.com/artist/Tupac-Shakur", "https://www.musixmatch.com/artist/Kanye-West", "https://www.musixmatch.com/artist/The-Black-Eyed-Peas"],
+    "total_vocab_len": 31894,
+    "avg_vocab": 6378.8,
+    "artists": ["Eminem", "Jay Z", "Tupac Shakur", "Kanye West", "The Black Eyed Peas"],
+    "num_artists": 5
+}, {
+    "genre_name": "Funk Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Red-Hot-Chili-Peppers"],
+    "total_vocab_len": 3636,
+    "avg_vocab": 3636.0,
+    "artists": ["Red Hot Chili Peppers"],
+    "num_artists": 1
+}, {
+    "genre_name": "Pop",
+    "share_urls": ["https://www.musixmatch.com/artist/Katy-Perry", "https://www.musixmatch.com/artist/Michael-Jackson", "https://www.musixmatch.com/artist/Lady-Gaga", "https://www.musixmatch.com/artist/Britney-Spears", "https://www.musixmatch.com/artist/Madonna", "https://www.musixmatch.com/artist/Elton-John", "https://www.musixmatch.com/artist/ABBA", "https://www.musixmatch.com/artist/Celine-Dion", "https://www.musixmatch.com/artist/Andrea-Bocelli", "https://www.musixmatch.com/artist/Barbra-Streisand", "https://www.musixmatch.com/artist/Billy-Joel", "https://www.musixmatch.com/artist/Frank-Sinatra-2", "https://www.musixmatch.com/artist/Barry-Manilow", "https://www.musixmatch.com/artist/Neil-Diamond", "https://www.musixmatch.com/artist/Cher", "https://www.musixmatch.com/artist/The-Carpenters", "https://www.musixmatch.com/artist/Olivia-Newton-John", "https://www.musixmatch.com/artist/Donna-Summer", "https://www.musixmatch.com/artist/George-Michael", "https://www.musixmatch.com/artist/The-Police", "https://www.musixmatch.com/artist/The-Bee-Gees", "https://www.musixmatch.com/artist/Backstreet-Boys", "https://www.musixmatch.com/artist/Lionel-Richie", "https://www.musixmatch.com/artist/Spice-Girls"],
+    "total_vocab_len": 59141,
+    "avg_vocab": 2464.2083333333335,
+    "artists": ["Katy Perry", "Michael Jackson", "Lady Gaga", "Britney Spears", "Madonna", "Elton John", "ABBA", "Celine Dion", "Andrea Bocelli", "Barbra Streisand", "Billy Joel", "Frank Sinatra", "Barry Manilow", "Neil Diamond", "Cher", "The Carpenters", "Olivia Newton-John", "Donna Summer", "George Michael", "The Police", "Bee Gees", "Backstreet Boys", "Lionel Richie", "Spice Girls"],
+    "num_artists": 24
+}, {
+    "genre_name": "Glam Metal",
+    "share_urls": ["https://www.musixmatch.com/artist/Motley-Crue"],
+    "total_vocab_len": 2343,
+    "avg_vocab": 2343.0,
+    "artists": ["M\u00f6tley Cr\u00fce"],
+    "num_artists": 1
+}, {
+    "genre_name": "Rock and Roll",
+    "share_urls": ["https://www.musixmatch.com/artist/Elvis-Presley"],
+    "total_vocab_len": 2531,
+    "avg_vocab": 2531.0,
+    "artists": ["Elvis Presley"],
+    "num_artists": 1
+}, {
+    "genre_name": "Teen Pop",
+    "share_urls": ["https://www.musixmatch.com/artist/New-Kids-On-the-Block"],
+    "total_vocab_len": 1802,
+    "avg_vocab": 1802.0,
+    "artists": ["New Kids on the Block"],
+    "num_artists": 1
+}, {
+    "genre_name": "Progressive Rock",
+    "share_urls": ["https://www.musixmatch.com/artist/Genesis", "https://www.musixmatch.com/artist/Pink-Floyd"],
+    "total_vocab_len": 6574,
+    "avg_vocab": 3287.0,
+    "artists": ["Genesis", "Pink Floyd"],
+    "num_artists": 2
+}, {
+    "genre_name": "Folk",
+    "share_urls": ["https://www.musixmatch.com/artist/Bob-Dylan"],
+    "total_vocab_len": 4883,
+    "avg_vocab": 4883.0,
+    "artists": ["Bob Dylan"],
+    "num_artists": 1
+}, {
+    "genre_name": "New Age",
+    "share_urls": ["https://www.musixmatch.com/artist/Enya"],
+    "total_vocab_len": 1869,
+    "avg_vocab": 1869.0,
+    "artists": ["Enya"],
+    "num_artists": 1
+}];
